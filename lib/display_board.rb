dd4663b28400b870234149e239b0a3be2a9387b3
jacob chaar <jacobchaar@live.ca>
2017-12-30 22:25:02 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board
-
+def display_board(board)
+  puts board[0]
+  puts board[1]
+  puts board[2]
+  puts board[3]
+  puts board[4]
 end
